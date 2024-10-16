8384d695e6ca5542274cae1468b0c6ccd13acacc
AlexandraHockett <alexandramandio@gmail.com>
2024-10-16 13:07:43 +0100

    Build Login Page and add NetflixSans font-family

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -10,7 +10,7 @@ const tabs = [
 export default function NavBar() {
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-16 py-6 flex item-center">
+      <div className="px-16 py-6 flex items-center">
         <img
           className="h-16"
           src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
