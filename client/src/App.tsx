d25398fd2ddb81f2574debd84bd0201cf15597c0
AlexandraHockett <alexandramandio@gmail.com>
2024-10-17 09:12:14 +0100

    Building the Watch Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,12 @@ import BrowsePage from "./pages/BrowsePage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import PlansPage from "./pages/PlansPage";
+import WatchPage from "./pages/WatchPage";
 
 function App() {
   return (
     <div>
-      <BrowsePage />
+      <WatchPage />
     </div>
   );
 }
--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -5,7 +5,8 @@ export default function MovieList() {
     <div className="px-12 mt-4 space-y-8">
       <div>
         <p className="text-black text-2xl font-semibold mb-4">Popular Shows</p>
-        <div className="grid grid-cols gap-2">
+        <div className="flex gap-2">
+          <MovieCard />
           <MovieCard />
         </div>
       </div>
