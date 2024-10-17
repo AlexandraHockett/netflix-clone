111edd3d92b1655829505792fc3c230c4f6d5abb
AlexandraHockett <alexandramandio@gmail.com>
2024-10-17 09:31:10 +0100

    Implementing Page Navigation

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
     <nav className="w-full fixed z-40">
       <div className="px-16 py-6 flex items-center">
         <img
-          className="h-16"
+          className="h-14"
           src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
           alt="Netflix logo"
         />
