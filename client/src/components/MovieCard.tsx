ea25052619f0f34e0037058c35b61580dbbb9b4d
AlexandraHockett <alexandramandio@gmail.com>
2024-10-17 13:19:55 +0100

    Passo :Consuming the Data in Our Browse Page

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,11 +1,14 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { PlayIcon } from "@heroicons/react/24/solid";
+import { Movie } from "../types";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }: { movie: Movie }) => {
+  const { thumbnailUrl, description, duration, title, genre } = movie;
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw] w-[24%]">
       <img
-        src="https://occ-0-3638-360.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABcg-6wxvemr1P31QyHelPC1cEsElblVVUcRNCpatOPaXWuKWrsbFfjM4C_oSjahYNk5w7MknnoCGQ-wLaSVaIonA3ARcVTfAr7w.webp?r=284"
+        src={thumbnailUrl}
         alt="Movie"
         draggable={false}
         className="
@@ -42,7 +45,7 @@ const MovieCard = () => {
       "
       >
         <img
-          src="https://occ-0-3638-360.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABcg-6wxvemr1P31QyHelPC1cEsElblVVUcRNCpatOPaXWuKWrsbFfjM4C_oSjahYNk5w7MknnoCGQ-wLaSVaIonA3ARcVTfAr7w.webp?r=284"
+          src={thumbnailUrl}
           alt="Movie"
           draggable={false}
           className="
@@ -77,17 +80,13 @@ const MovieCard = () => {
               <ChevronDownIcon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
             </div>
           </div>
-          <p className="text-white font-semibold mt-4 text-2xl">minions 2</p>
-          <p className="text-gray-400">
-            Clever kid Gru wants to be a supervillain. The Minions want a boss.
-            It's the perfect match: Even bad guys need a little a help from
-            their friends.
-          </p>
+          <p className="text-white font-semibold mt-4 text-2xl">{title}</p>
+          <p className="text-gray-400">{description}</p>
           <div className="flex flex-row mt-4 gap-2 items-center">
-            <p className="text-white text-[10px] lg:text-sm">87 m</p>
+            <p className="text-white text-[10px] lg:text-sm">{duration}</p>
           </div>
           <div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
-            <p>Kids & Family Movies</p>
+            <p>{genre}</p>
           </div>
         </div>
       </div>
