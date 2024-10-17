2d82a2ae88eb2f4ae6ec9cd629c8c05448d3b325
AlexandraHockett <alexandramandio@gmail.com>
2024-10-17 14:06:59 +0100

    Navigating to the Watch Page

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const tabs = [
   "Home",
   "TV Shows",
@@ -8,9 +10,25 @@ const tabs = [
 ];
 
 export default function NavBar() {
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 700) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    });
+  }, []);
+
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-16 py-6 flex items-center">
+      <div
+        className={`px-16 py-6 flex items-center ${
+          showBackground ? "bg-black bg-opacity-90" : null
+        }`}
+      >
         <img
           className="h-14"
           src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
