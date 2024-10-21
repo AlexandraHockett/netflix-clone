2861e6540afc33a49d3ebce9327572efb2de8c6d
AlexandraHockett <alexandramandio@gmail.com>
2024-10-21 13:14:49 +0100

    corrected form card layout, integrated React Hook Form, and set up useForm hook

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,26 +1,70 @@
+import { useState } from "react";
 import Input from "../components/Input";
 import NavBar from "../components/NavBar";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  name: string;
+  password: string;
+};
+
+enum Variant {
+  SIGN_UP,
+  LOGIN_IN,
+}
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<Inputs>();
+  const [variant, setVariant] = useState(Variant.LOGIN_IN);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
       <NavBar />
       <div className="flex justify-center items-center h-full">
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
           <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
-          <form className="flex flex-col gap-4">
-            <Input />
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {variant === Variant.SIGN_UP && (
+              <Input id="username" type="text" label="Username" />
+            )}
+            <Input id="email" type="email" label="Email address" />
+            <Input id="password" type="password" label="Password" />
             <input
               value={"Submit"}
               type="submit"
               className="bg-red-400 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
             />
           </form>
-          <p className="text-neutral-500 mt-12">
-            <span className="text-white ml-1 hover:underline cursor-pointer">
-              First time using Netflix?
-            </span>
-          </p>
+          {variant === Variant.LOGIN_IN ? (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.SIGN_UP)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                First time using Netflix?
+              </span>
+            </p>
+          ) : (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.LOGIN_IN)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                Already have an account?
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
