bbe350a98abe92051c6c49d9290f256a77da7d1f
AlexandraHockett <alexandramandio@gmail.com>
2024-10-24 15:50:09 +0100

    Redirect User to the Plan Page if They Don't Have a Subscription

--- client/src/hooks/useMoviesList.ts
+++ client/src/hooks/useMoviesList.ts
@@ -1,6 +1,9 @@
 import { useEffect, useReducer, useState } from "react";
-import axios from "axios";
 import { Movie } from "../types";
+import Cookie from "universal-cookie";
+import axios from "axios";
+
+const cookie = new Cookie();
 
 interface State {
   data: Movie[] | null;
@@ -63,19 +66,31 @@ const useMoviesList = (offset: number) => {
   }, [offset]);
 
   const fetchMoviesList = async () => {
+    const sessionToken = cookie.get("session_token");
     if (data && count && data.length >= count) return;
     dispatch({ type: ActionType.LOADING });
     try {
       const response = await axios.get(
-        `http://localhost:8080/movies/list?offset=${offset}`
+        `http://localhost:8080/movies/list?offset=${offset}`,
+        {
+          headers: {
+            ...(sessionToken
+              ? { Authorization: `Bearer ${sessionToken}` }
+              : null),
+          },
+        }
       );
       const moviesData = data
         ? [...data, ...response.data.movies]
         : response.data.movies;
       setCount(response.data.count);
       dispatch({ type: ActionType.SUCCESS, payload: moviesData });
-    } catch (error) {
-      dispatch({ type: ActionType.FAILED, payload: "Something went wrong" });
+    } catch (error: any) {
+      console.log(error);
+      dispatch({
+        type: ActionType.FAILED,
+        payload: error?.response?.data?.errors[0].msg,
+      });
     }
   };
   return { data, loading, error };
